b41a44daf567fd3f6bbfcf51aac128f408787083
Junge <201305945@post.au.dk>
2019-02-14 17:59:41 +0100

    #6 - Enabling CORS

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import app from "./app";
+import * as cors from "cors";
+
 const PORT = 3000;
 
+// Enable cors - default (any) till we know where it will be hosted
+app.use(cors());
+
+// Listen on port
 app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
 })
